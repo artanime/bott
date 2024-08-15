7c9d854b36b2cebeda086224ae0c398783e59135
artanime <artanimecustomization@gmail.com>
2024-08-15 02:25:49 -0800

    comit

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -3,12 +3,10 @@ import './ProductList.css';
 import ProductItem from "../ProductItem/ProductItem";
 
 const products = [
-    { id: '1', title: 'Аттака Титанов', price: 5000, description: 'Синего цвета, прямые', image: 'Photo/1.jpg' },
-    { id: '2', title: 'Наруто', price: 12000, description: 'Зеленого цвета, теплая', image: 'Photo/2.jpg' },
-    { id: '3', title: 'Город', price: 5000, description: 'Синего цвета, прямые', image: 'Photo/3.jpg' },
-    { id: '4', title: 'Аватар', price: 122, description: 'Зеленого цвета, теплая', image: 'Photo/4.jpg' },
-    { id: '5', title: 'Легенда о Коре', price: 5000, description: 'Синего цвета, прямые', image: 'Photo/5.jpg' },
-    { id: '6', title: 'Легенда о Коре', price: 5000, description: 'Синего цвета, прямые', image: 'Photo/6.jpg' },
+    { id: '1', title: 'Аттака Титанов', price: 5000, description: 'Синего цвета, прямые', image: 'Photo/attack.jpg' },
+    { id: '2', title: 'Наруто', price: 12000, description: 'Зеленого цвета, теплая', image: 'Photo/2.1.jpg' },
+    { id: '3', title: 'Город', price: 5000, description: 'Синего цвета, прямые', image: 'Photo/3.1.jpg' },
+    { id: '4', title: 'Аватар', price: 122, description: 'Зеленого цвета, теплая', image: 'Photo/4.1jpg' },
 ];
 
 const getTotalPrice = (items = []) => {
