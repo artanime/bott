9d6d0439c1cafe982b431c895bce929606f25d47
artanime <artanimecustomization@gmail.com>
2024-09-09 01:36:10 -0800

    2

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,11 +7,19 @@ const Header = () => {
     const {user, onClose} = useTelegram();
 
     return (
-        <div className={'header'}>
-            <Button onClick={onClose}>Закрыть</Button>
-            <span className={'username'}>
-                {user?.username}
-            </span>
+        <div className="header">
+            <div className="header-left">
+                <img src="/Logo/logo.png" alt="App Logo" className="logo" /> {/* Логотип приложения */}
+                <select className="language-selector">
+                    <option value="ru">Русский</option>
+                    <option value="en">English</option>
+                    <option value="ua">Українська</option>
+                </select>
+            </div>
+            <div className="header-right">
+                <Button onClick={onClose}>Закрыть</Button>
+                <span className="username">{user?.username}</span>
+            </div>
         </div>
     );
 };
